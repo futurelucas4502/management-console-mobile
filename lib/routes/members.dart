9884a91db133516f156b4a1383faef9217b04876
Harrison Elliott <harrisongeoelliott@gmail.com>
2020-05-20 18:05:53 +0100

    Inital Commit - Add Members

---
+++ lib/routes/members.dart
@@ -0,0 +1,18 @@
+// members.dart
+// Changelog:
+// 20-05: Initial Code / Commit - Harrison
+
+// Import required libaries / packages
+import 'package:flutter/material.dart';
+import './main.dart' as globals;
+
+// Start MembersPage
+
+class MembersPage extends StatefulWidget {
+  @override
+  _MembersPageState createState() => _MembersPageState();
+}
+
+class _MembersPageState extends State<MembersPage> {
+
+}
